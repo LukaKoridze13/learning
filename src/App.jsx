b4a6ea1caed39aa68b8415706cfd1c1f6736733d
Luka Koridze <lukakoridze13@gmail.com>
2026-01-31 18:22:13 +0400

    Replace App placeholder with lorem ipsum
    Replace the simple <div>App</div> with a longer lorem ipsum paragraph to serve as placeholder content in the App component. No structural changes to the component were made; this change is intended for layout/content preview during development.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nam quidem dolore veritatis natus laudantium sint perspiciatis ipsa ipsam praesentium, optio cum voluptate voluptatibus soluta recusandae dolor facere odio deserunt.
+    </div>
   )
 }
 
