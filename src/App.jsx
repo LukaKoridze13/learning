01d242c506ba092a5cb37f5f1dc10e40d0170768
Luka Koridze <lukakoridze13@gmail.com>
2026-01-31 18:29:39 +0400

    Replace placeholder text with 'luka'

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem nam quidem dolore veritatis natus laudantium sint perspiciatis ipsa ipsam praesentium, optio cum voluptate voluptatibus soluta recusandae dolor facere odio deserunt.
+     luka
     </div>
   )
 }
